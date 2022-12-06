4776b4bd3837936249ab5a7ea5eedd89e7bd9cf0
burki <burki@DESKTOP-NQ13GS4>
2022-12-06 18:55:52 +0300

    GenericRepository and GenericDal parts insert

--- DataAccessLayer/Repositories/CategoryRepository.cs
+++ DataAccessLayer/Repositories/CategoryRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,34 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Repositories
 {
-    public class CategoryRepository
+    public class CategoryRepository : ICategoryDal
     {
+        Context c = new Context();
+        public void AddCategory(Category category)
+        {
+            c.Add(category);
+            c.SaveChanges();    
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            c.Remove(category);
+            c.SaveChanges();
+        }
+
+        public Category GetById(int id)
+        {
+            return c.Categories.Find(id);
+        }
+
+        public List<Category> ListAllCategory()
+        {
+            return c.Categories.ToList();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            c.Update(category);
+        }
     }
 }
