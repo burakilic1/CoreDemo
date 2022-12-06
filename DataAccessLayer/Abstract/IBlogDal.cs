395258479a2b87cb85762408f8ae7bf48daa428b
burki <burki@DESKTOP-NQ13GS4>
2022-12-06 16:45:34 +0300

    Abtract and repositories

--- DataAccessLayer/Abstract/IBlogDal.cs
+++ DataAccessLayer/Abstract/IBlogDal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    internal interface IBlogDal
+    public interface IBlogDal
     {
+        List<Blog> ListAllBlog();
+        void AddBlog(Blog blog);
+        void DeleteBlog(Blog blog);
+        void UpdateBlog(Blog blog);
+        Blog GetById(int id);
     }
 }
--- DataAccessLayer/Repositories/CategoryRepository.cs
+++ DataAccessLayer/Repositories/CategoryRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Repositories
 {
-    internal class CategoryRepository
+    public class CategoryRepository
     {
     }
 }
