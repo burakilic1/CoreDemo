0e711311e99ce281ff2cd6d597561fc928aa8be5
burki <burki@DESKTOP-NQ13GS4>
2022-12-09 10:43:00 +0300

    Comment Component adding

---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,32 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents
+{
+	public class CommentList : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var commentvalues = new List<UserComment>
+			{
+				new UserComment
+				{
+					ID=1,
+					UserName="ali"
+				},
+				new UserComment
+				{
+					ID=2,
+					UserName="mehmet"
+				},
+				new UserComment
+				{
+					ID=3,
+					UserName="yılmaz"
+				}
+
+			};
+			return View(commentvalues);
+		}
+	}
+}
